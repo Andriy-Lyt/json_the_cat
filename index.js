c998dec65bdaec74fb6982f82db525d513aea60f
Andriy-Lyt <howdy_eh@icloud.com>
2021-08-27 18:53:44 -0400

    final version

--- index.js
+++ index.js
@@ -1,34 +1,10 @@
 const { fetchBreedDescription } = require('./breedFetcher');
 
 let breedName = process.argv[2];
-if (!breedName) {
-  console.log('no breedName provided');
-  return;
-}
-if (breedName && breedName.length > 3) {
-  breedName = breedName.slice(0, 3);
-}
-// console.log("breedName: ", breedName);
-
-fetchBreedDescription(breedName, (error, data) => {
-  // console.log("error: ", error);
-  // console.log("data.length: ", data.length);
-
-  //check for errors
-  if (error !== null) { 
-    let err1 = 'Incorrect address provided.\n'+ error;
-    console.log(err1);
-    return err1;
-  }; 
-  if (data.length === 2) {
-    // console.log('body.length: ', data.length);
-    let resp2 = "invalid breed name provided";
-    console.log(resp2);
-    return resp2;
+fetchBreedDescription(breedName, (error, desc) => {
+  if (error) {
+    console.log("error fetch details: ", error);
+  } else {
+    console.log("desc: ", desc);
   }
-  //if no error
-  const parsedData =  JSON.parse(data);  
-  const result = parsedData[0].description;
-  console.log(result);
-  return result;
 });
--- test/breedFetcherTest.js
+++ test/breedFetcherTest.js
@@ -3,6 +3,7 @@ const { assert } = require('chai');
 
 describe('fetchBreedDescription', () => {
   it('returns a string description for a valid breed, via callback', (done) => {
+
     fetchBreedDescription('Siberian', (err, desc) => {
       
       // we expect no error for this scenario
@@ -11,7 +12,7 @@ describe('fetchBreedDescription', () => {
       const expectedDesc = "The Siberians dog like temperament and affection makes the ideal lap cat and will live quite happily indoors. Very agile and powerful, the Siberian cat can easily leap and reach high places, including the tops of refrigerators and even doors.";
 
       // compare returned description
-      assert.equal(expectedDesc, desc);
+      assert.equal(expectedDesc, desc.trim());
 
       done();
     });
